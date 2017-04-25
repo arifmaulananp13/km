b5455414b4d6073f2543dba4e3a92e8c02714880
arifmaulananp13 <arifmaulananp13@gmail.com>
2017-04-25 16:56:10 -0700

    user level fixed

--- application/controllers/Baprps.php
+++ application/controllers/Baprps.php
@@ -21,15 +21,6 @@ class Baprps extends CI_Controller {
 		$data['data'] = $this->m_inputbap->verif_data($where,'input_jadwal')->result();
 		view('rpsbap/input_bap',$data);
 		}
-		elseif($this->session->userdata('level') == 'Ketua Kelompok Keahlian'){
-		$data['title'] = "Input BAP";
-		$data['active_1'] = "active";
-		$data['data']=$this->m_inputbap->jadwal();
-		view('rpsbap/input_bap',$data);
-		}
-		elseif($this->session->userdata('level') == 'Sekretaris Kaprodi'){
-		redirect('baprps/forum');
-		} 	
 		elseif($this->session->userdata('level') == 'Mahasiswa'){
 		redirect('baprps/verif_bap');
 		}
@@ -37,7 +28,12 @@ class Baprps extends CI_Controller {
 		redirect('baprps/forum');
 		}		
 		else{
-		redirect('baprps/grafik');	
+		$data['title'] = "Input BAP";
+		$data['active_1'] = "active";
+		$dosen = $this->session->userdata('nama_user');
+		$where = array('dosen' => $dosen);
+		$data['data'] = $this->m_inputbap->verif_data($where,'input_jadwal')->result();
+		view('rpsbap/input_bap',$data);
 		}
 		}
 	
