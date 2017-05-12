5339b25400498f18b326f4bd3648a97ebadc56b8
arifmaulananp13 <arifmaulananp13@gmail.com>
2017-05-11 23:53:48 -0700

    fix combobox ngelink dari agung

--- application/views/rpsbap/1objek.php
+++ application/views/rpsbap/1objek.php
@@ -5,44 +5,81 @@
           </h1>
         </section>
         <section class="content">
-		  <div class="col-md-12 with-padding">
-        <form action="<?php echo base_url(). 'baprps/aksi_satuobjek'; ?>" method="post" class="box box-primary">
-        	<div class="box-header with-border">
-        		Pilih Objek
-        	</div>
-        	<div class="box-body">
-			<div class="col-md-6 form-group">
-            <div class="col-md-12 form-group">
-              <select name="dosen" id="dosen" class="form-control" required="">
-                  <option disabled="" selected="">Dosen</option>
-				  <?php foreach($dosen as $d){ ?>
-				  <option value="<?php echo $d['nama_dosen']; ?>"><?php echo $d['nama_dosen']; ?></option>
-				  <?php } ?>
-                </select>
+          <div class="col-md-12 with-padding">
+            <form action="<?php echo base_url(). 'baprps/aksi_satuobjek'; ?>" method="post" class="box box-primary">
+             <div class="box-header with-border">
+              Pilih Objek
             </div>
-	        	<div class="col-md-12 form-group">
-		        	<select name="matkul" id="matkul" class="form-control" required="">
-		            <option disabled="" selected="">Mata Kuliah</option>
-					<?php foreach($matkul as $m) {?>
-					<option value="<?php echo $m['nama_matkul']; ?>"><?php echo $m['nama_matkul']; ?></option>
-					<?php } ?> 					
-		            </select>
-	        	</div>
-	        	<div class="col-md-12 form-group">
-		        	<select name="kelas" id="kelas" class="form-control" required="">
-		            <option disabled="" selected="">Kelas</option> 	
-					<?php foreach($kelas as $k) {?>
-					<option value="<?php echo $k['nama_kelas']; ?>"><?php echo $k['nama_kelas']; ?></option>
-					<?php } ?> 					
-		            </select>
-	        	</div>
+            <div class="box-body">
+             <div class="col-md-6 form-group">
+              <div class="col-md-12 form-group">
+                <select name="dosen" id="dosen" class="form-control" required="">
+                  <option disabled="" selected="">Dosen</option>
+                  <?php foreach($dosen as $d){ ?>
+                  <option value="<?php echo $d['dosen']; ?>"><?php echo $d['dosen']; ?></option>
+                  <?php } ?>
+                </select>
+              </div>
+              <div class="col-md-12 form-group">
+               <select name="matkul" id="matkul" class="form-control" required="">
+                <option disabled="" selected="">Mata Kuliah</option>				
+              </select>
+            </div>
+            <div class="col-md-12 form-group">
+             <select name="kelas" id="kelas" class="form-control" required="">
+              <option disabled="" selected="">Kelas</option> 
+            </select>
+          </div>
+          <div class="col-md-12 form-group">
+           <button name="mysubmit" class="btn btn-primary pull-left btn-flat" type="submit">Show Record</button>
+         </div>
+       </div>
+     </div>
+   </form>
+ </div>
+</section>
 
-				
-        		<div class="col-md-12 form-group">
-        			<button name="mysubmit" class="btn btn-primary pull-left btn-flat" type="submit">Show Record</button>
-        		</div>
-        	</div>
-			</div>
-        </form>
-        </div>
-        </section>
+<script>
+  $(document).ready(function(){
+    $("#dosen").on('change', function(){
+      $.ajax({
+        url: "/km/api/list_matkul",
+        type: "get",
+        data: {
+          dosen: $("#dosen").val()
+        },
+        success: function(response) {
+          $('#matkul').html('<option disabled="" selected="">Mata Kuliah</option>');
+          if(response.length > 0){
+            response.forEach(function(value){
+               $('#matkul').append('<option value="'+value.matkul+'">'+value.matkul+'</option>');
+            })
+          }
+        },
+        error: function(xhr) {
+        }
+      });
+    });
+
+    $("#matkul").on('change', function(){
+      $.ajax({
+        url: "/km/api/list_kelas",
+        type: "get",
+        data: {
+          dosen: $("#dosen").val(),
+          matkul: $("#matkul").val()
+        },
+        success: function(response) {
+          $('#kelas').html('<option disabled="" selected="">Kelas</option>');
+          if(response.length > 0){
+            response.forEach(function(value){
+               $('#kelas').append('<option value="'+value.kelas+'">'+value.kelas+'</option>');
+            })
+          }
+        },
+        error: function(xhr) {
+        }
+      });
+    });
+  });
+</script>
