9dc01809733644259d93d1572c78e8f46750ee01
arifmaulananp13 <arifmaulananp13@gmail.com>
2017-05-13 12:20:01 -0700

    add combo box ngelink di tabel 2 object

--- application/views/rpsbap/grafik.php
+++ application/views/rpsbap/grafik.php
@@ -12,55 +12,43 @@
         	</div>
         	<div class="box-body">
 			<div class="col-md-6 form-group">
+              <div class="col-md-12 form-group">
+                <select name="dosen" id="dosen" class="form-control" required="">
+                  <option disabled="" selected="">Dosen</option>
+                  <?php foreach($dosen as $d){ ?>
+                  <option value="<?php echo $d['dosen']; ?>"><?php echo $d['dosen']; ?></option>
+                  <?php } ?>
+                </select>
+              </div>
+              <div class="col-md-12 form-group">
+               <select name="matkul" id="matkul" class="form-control" required="">
+                <option disabled="" selected="">Mata Kuliah</option>				
+              </select>
+            </div>
             <div class="col-md-12 form-group">
-              <select name="dosen" id="dosen" class="form-control" required="">
-                  <option disabled="" selected="">Dosen</option>
-				  <?php foreach($dosen as $d){ ?>
-				  <option value="<?php echo $d['nama_dosen']; ?>"><?php echo $d['nama_dosen']; ?></option>
-				  <?php } ?>
-                </select>
-            </div>
-	        	<div class="col-md-12 form-group">
-		        	<select name="matkul" id="matkul" class="form-control" required="">
-		            <option disabled="" selected="">Mata Kuliah</option>
-					<?php foreach($matkul as $m) {?>
-					<option value="<?php echo $m['nama_matkul']; ?>"><?php echo $m['nama_matkul']; ?></option>
-					<?php } ?> 					
-		            </select>
-	        	</div>
-	        	<div class="col-md-12 form-group">
-		        	<select name="kelas" id="kelas" class="form-control" required="">
-		            <option disabled="" selected="">Kelas</option> 	
-					<?php foreach($kelas as $k) {?>
-					<option value="<?php echo $k['nama_kelas']; ?>"><?php echo $k['nama_kelas']; ?></option>
-					<?php } ?> 					
-		            </select>
-	        	</div>
+             <select name="kelas" id="kelas" class="form-control" required="">
+              <option disabled="" selected="">Kelas</option> 
+            </select>
+          </div>
 			<label for="dosen1" class="form-label">Compare dengan :</label>	
-			<div class="col-md-12 form-group">
-              <select name="dosen1" id="dosen1" class="form-control" required="">
+              <div class="col-md-12 form-group">
+                <select name="dosen1" id="dosen1" class="form-control" required="">
                   <option disabled="" selected="">Dosen</option>
-				  <?php foreach($dosen as $d){ ?>
-				  <option value="<?php echo $d['nama_dosen']; ?>"><?php echo $d['nama_dosen']; ?></option>
-				  <?php } ?>
+                  <?php foreach($dosen as $d){ ?>
+                  <option value="<?php echo $d['dosen']; ?>"><?php echo $d['dosen']; ?></option>
+                  <?php } ?>
                 </select>
+              </div>
+              <div class="col-md-12 form-group">
+               <select name="matkul1" id="matkul1" class="form-control" required="">
+                <option disabled="" selected="">Mata Kuliah</option>				
+              </select>
             </div>
-	        	<div class="col-md-12 form-group">
-		        	<select name="matkul1" id="matkul1" class="form-control" required="">
-		            <option disabled="" selected="">Mata Kuliah</option>
-					<?php foreach($matkul as $m) {?>
-					<option value="<?php echo $m['nama_matkul']; ?>"><?php echo $m['nama_matkul']; ?></option>
-					<?php } ?> 					
-		            </select>
-	        	</div>
-	        	<div class="col-md-12 form-group">
-		        	<select name="kelas1" id="kelas1" class="form-control" required="">
-		            <option disabled="" selected="">Kelas</option> 	
-					<?php foreach($kelas as $k) {?>
-					<option value="<?php echo $k['nama_kelas']; ?>"><?php echo $k['nama_kelas']; ?></option>
-					<?php } ?> 					
-		            </select>
-	        	</div>
+            <div class="col-md-12 form-group">
+             <select name="kelas1" id="kelas1" class="form-control" required="">
+              <option disabled="" selected="">Kelas</option> 
+            </select>
+          </div>
 				
 				
         		<div class="col-md-12 form-group">
@@ -71,3 +59,93 @@
         </form>
         </div>
         </section>
+		
+<script>
+  $(document).ready(function(){
+    $("#dosen").on('change', function(){
+      $.ajax({
+        url: "/km/api/list_matkul",
+        type: "get",
+        data: {
+          dosen: $("#dosen").val()
+        },
+        success: function(response) {
+          $('#matkul').html('<option disabled="" selected="">Mata Kuliah</option>');
+          if(response.length > 0){
+            response.forEach(function(value){
+               $('#matkul').append('<option value="'+value.matkul+'">'+value.matkul+'</option>');
+            })
+          }
+        },
+        error: function(xhr) {
+        }
+      });
+    });
+
+    $("#matkul").on('change', function(){
+      $.ajax({
+        url: "/km/api/list_kelas",
+        type: "get",
+        data: {
+          dosen: $("#dosen").val(),
+          matkul: $("#matkul").val()
+        },
+        success: function(response) {
+          $('#kelas').html('<option disabled="" selected="">Kelas</option>');
+          if(response.length > 0){
+            response.forEach(function(value){
+               $('#kelas').append('<option value="'+value.kelas+'">'+value.kelas+'</option>');
+            })
+          }
+        },
+        error: function(xhr) {
+        }
+      });
+    });
+  });
+</script>
+
+<script>
+  $(document).ready(function(){
+    $("#dosen1").on('change', function(){
+      $.ajax({
+        url: "/km/api/list_matkul",
+        type: "get",
+        data: {
+          dosen: $("#dosen1").val()
+        },
+        success: function(response) {
+          $('#matkul1').html('<option disabled="" selected="">Mata Kuliah</option>');
+          if(response.length > 0){
+            response.forEach(function(value){
+               $('#matkul1').append('<option value="'+value.matkul+'">'+value.matkul+'</option>');
+            })
+          }
+        },
+        error: function(xhr) {
+        }
+      });
+    });
+
+    $("#matkul1").on('change', function(){
+      $.ajax({
+        url: "/km/api/list_kelas",
+        type: "get",
+        data: {
+          dosen: $("#dosen1").val(),
+          matkul: $("#matkul1").val()
+        },
+        success: function(response) {
+          $('#kelas1').html('<option disabled="" selected="">Kelas</option>');
+          if(response.length > 0){
+            response.forEach(function(value){
+               $('#kelas1').append('<option value="'+value.kelas+'">'+value.kelas+'</option>');
+            })
+          }
+        },
+        error: function(xhr) {
+        }
+      });
+    });
+  });
+</script>
