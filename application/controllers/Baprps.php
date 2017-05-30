11b594f003bbb97c3d6c8228e2b9b8155c38292f
arifmaulananp13 <arifmaulananp13@gmail.com>
2017-05-30 10:12:22 -0700

    Revert "cuma ngetes"
    This reverts commit ad782792917135db415d36a2c996b0b59e471ee7.

--- application/controllers/Baprps.php
+++ application/controllers/Baprps.php
@@ -19,7 +19,6 @@ class Baprps extends CI_Controller {
 		$dosen = $this->session->userdata('nama_user');
 		$where = array('dosen' => $dosen);
 		$data['data'] = $this->m_inputbap->verif_data($where,'input_jadwal')->result();
-		$data['pertemuan'] = $this->combobox_model->getPertemuanAll();		
 		view('rpsbap/input_bap',$data);
 		}
 		elseif($this->session->userdata('level') == 'Mahasiswa'){
@@ -42,8 +41,8 @@ class Baprps extends CI_Controller {
 		$data['title'] = "Input BAP";
 		$where = array('id_jadwal' => $id);
 		$data['data'] = $this->m_inputbap->verif_data($where,'input_jadwal')->result();
-		$pertemuan = $this->input->post('pertemuan');
-		$data['kode_matkul'] = $this->m_inputbap->getrps($kode_matkul,$pertemuan)->result();
+		$data['kode_matkul'] = $this->m_inputbap->getrps($kode_matkul)->result();
+		$data['pertemuan'] = $this->combobox_model->getPertemuanAll();
 		view('rpsbap/input_bap2',$data);	
 	}
 
